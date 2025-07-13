2ea5e4961121791a369ad8edea4384e4e4822755
1830488003 <1830488003@qq.com>
2025-07-14 01:52:07 +0800

    1

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 // 创建 Express 应用实例
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'qq67564534';
 
 // 使用中间件
+app.use(cors()); // 允许所有来源的跨域请求，方便插件调用
 app.use(express.json()); // 解析请求体中的JSON数据
 
 
